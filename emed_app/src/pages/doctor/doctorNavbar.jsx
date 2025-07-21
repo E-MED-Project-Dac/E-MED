df0eb7cdaf7a75efb3ff26c8b5cf6a0a9e55ab57
89193-PulkitMehta <pulkitt522@gmail.com>
2025-07-21 19:13:06 +0530

    doctorNavbar

--- emed_app/src/pages/doctor/doctorNavbar.jsx
+++ emed_app/src/pages/doctor/doctorNavbar.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
 
-function doctorNavbar() {
+function DoctorNavbar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className="navbar navbar-expand-lg bg-success" data-bs-theme="dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand fs-5" to="/home">
+          Home
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav gap-4 fs-5">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/home/profile">
+                Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/home/appointment">
+                Appointment
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto fs-5">
+            <li className="nav-item">
+              <Link className="btn btn-outline-light px-4" to="/home/logout">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+  );
 }
-
-export default doctorNavbar
+export default DoctorNavbar;
