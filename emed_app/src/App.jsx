3c312d96b55109d535841a5db4fd2772c50ae86c
saransh <saranshvats041@gmail.com>
2025-07-22 16:25:20 +0530

    After resolving conflicts

--- emed_app/src/App.jsx
+++ emed_app/src/App.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import { ToastContainer } from "react-toastify";
-import PatientNavbar from "./pages/patient/PatientNavbar";
-import PatientProfile from "./pages/patient/PatientProfile";
-import DoctorEdit from "./pages/doctor/DoctorEdit";
-import DoctorNavbar from "./pages/doctor/doctorNavbar";
+import React, { useState } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import { ToastContainer } from 'react-toastify'
+import PatientNavbar from './pages/patient/PatientNavbar'
+import PatientProfile from './pages/patient/PatientProfile'
+import DoctorProfile from './pages/doctor/DoctorProfile'
 //import { AuthContext } from './contexts/auth.context'
 function App() {
   // create a state member for keeping user details
@@ -14,14 +13,28 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="patientNavbar" element={<PatientNavbar />} />
-        <Route path="patientProfile" element={<PatientProfile />} />
-        <Route path="editDoctor" element={<DoctorEdit />} />
-        <Route path="doctorNavbar" element={<DoctorNavbar />} />
-      </Routes>
+        <Routes>
+          <Route
+            path='/'
+            element={<Login />}
+          />
+          <Route
+            path='register'
+            element={<Register />}
+          />
+          <Route 
+            path='patientNavbar'
+            element={<PatientNavbar/>}
+          />
+          <Route 
+            path='patientProfile'
+            element={<PatientProfile/>}
+          />
+          <Route
+            path='doctorProfile'
+            element={<DoctorProfile/>}
+          />
+        </Routes>
 
       <ToastContainer />
     </>
---
+++ emed_app/src/pages/doctor/DoctorProfile.jsx
@@ -0,0 +1,104 @@
+import React , {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import './DoctorProfile.css';
+
+function DoctorProfile(){
+  const navigate = useNavigate();
+  const [showDeleteConfirmation,setShowDeleteConfirmation] = useState(false);
+  const onUpdate = () => {
+    navigate('');
+  };
+
+  const onBack = () => {
+    navigate(-1);
+  }
+
+  const onDeleteAccount = () => {
+    console.log("Account Deleted!!!");
+    navigate('/');
+  };
+
+  const onCancelDelete = () => {
+    setShowDeleteConfirmation(false);
+  }
+
+  return(
+    <>
+        <div className="container">
+            <h2 className='page-header'>Profile</h2>
+               <div className="profile-content">
+                 <div className="profile-image-container">
+                   <img 
+                    src=""
+                    alt="Profile-image" 
+                    className="profile-image"
+                   />   
+                 </div>
+                 <div style={{textAlign:'center'}}>
+                      <button className="btn update-img"  >Update Image</button>
+                   </div>
+          
+                 <div className="profile-details">
+                  <h3>Saransh</h3>
+                  <p><strong>Email:</strong> saranshvats041@gmail.com</p>
+                  <p><strong>Bio:</strong> abc</p>
+                 </div>
+                </div>
+                <div className="profile-actions">
+                  <button 
+                   onClick={onUpdate}
+                   className="btn update-btn"
+                  >
+                  Update Basic Details
+                  </button>
+
+                   <button 
+                   onClick={onDeleteAccount}
+                   className="btn update-btn"
+                  >
+                  Avaialability Details
+                 </button>
+          
+                  <button 
+                   onClick={onDeleteAccount}
+                   className="btn delete-btn"
+                  >
+                  Delete Account
+                 </button>
+          
+                 <button 
+                 onClick={onBack}
+                 className="btn back-btn"
+                 >
+                  Back
+                 </button>
+                </div>
+                 {showDeleteConfirmation && (
+                   <div className="modal-delete">
+                     <div className="delete-content">
+                       <h3>Confirm Account Deletion</h3>
+                       <p>Are you sure you want to delete your account? This action cannot be undone.</p>
+                       <div className="delete-actions">
+                         <button 
+                          onClick={onConfirmDelete}
+                          className="btn confirm-delete-btn"
+                         >
+                          Yes, Delete
+                         </button>
+                         <button 
+                          onClick={onCancelDelete}
+                          className="btn cancel-delete-btn"
+                         >
+                         Cancel, Delete
+                         </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+        </div>
+    </>
+  )
+
+}
+
+export default DoctorProfile
