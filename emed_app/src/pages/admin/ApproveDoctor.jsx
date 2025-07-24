8ac245aa8610be6a766d4c93276e8c403ded0fc1
saransh <saranshvats041@gmail.com>
2025-07-24 17:34:10 +0530

    admin changes

---
+++ emed_app/src/pages/admin/ApproveDoctor.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DoctorProfileBeforeVerifying from './BeforeVerifying';
+function ApproveDoctor(){
+  const navigate = useNavigate();
+  const [registeredDoctors,setRegisteredDoctors]=useState([
+    {
+      SrNo:"1",
+      DoctorName:"Dr. Gurvindar",
+      EmailId:"guru123@gmail.com",
+      Specialization:"Cardiologist",
+    },
+    {
+      SrNo:"2",
+      DoctorName:"Dr. Saransh",
+      EmailId:"saransh041@gmail.com",
+      Specialization:"Neurologist",
+    },
+    {
+      SrNo:"3",
+      DoctorName:"Dr. Pulkit",
+      EmailId:"pulkit321@gmail.com",
+      Specialization:"Orthologist",
+    },
+    {
+      SrNo:"4",
+      DoctorName:"Dr. Aadarsh",
+      EmailId:"aadarsh@gmail.com",
+      Specialization:"Cardiologist",
+    },
+  ]);
+
+  const onViewDetails = () =>{
+    navigate('/beforeVerifying');
+  }
+  return(
+<>
+     <div>
+        <h2 style={{textAlign:'center'}}>Newly Registered Doctors</h2>
+        <hr/>
+        <div style={{margin:30}}>
+          <table className='table table-stripped border-1'>
+            <thead>
+              <tr>
+                <th>SrNo.</th>
+                <th>Name</th>
+                <th>EmailId</th>
+                <th>Specialization</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {registeredDoctors.map((doctor) => (
+                <tr>
+                  <td>{doctor.SrNo}</td>
+                  <td>{doctor.DoctorName}</td>
+                  <td>{doctor.EmailId}</td>
+                  <td>{doctor.Specialization}</td>
+                  <td>
+                    <div>
+                      <button 
+                        className='btn btn-info'
+                        onClick={onViewDetails}
+                      >
+                        View Details
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div>
+            <button
+              className='btn btn-warning'
+              onClick={''}
+            >
+                Back
+            </button>
+          </div>
+        </div>
+     </div>
+</>
+  );
+}
+export default ApproveDoctor;
