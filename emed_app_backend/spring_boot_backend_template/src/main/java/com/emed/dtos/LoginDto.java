9c495f316332292bb556f93fc7a344e8386a53f0
adarshsingh <adarshmohit2310@gmail.com>
2025-08-03 14:50:28 +0530

    LoginRegisterDto_Adarsh

---
+++ emed_app_backend/spring_boot_backend_template/src/main/java/com/emed/dtos/LoginDto.java
@@ -0,0 +1,8 @@
+package com.emed.dtos;
+
+public class LoginDto {
+
+	private String email;
+
+    private String password;
+}
---
+++ emed_app_backend/spring_boot_backend_template/src/main/java/com/emed/dtos/RegisterDto.java
@@ -0,0 +1,18 @@
+package com.emed.dtos;
+
+import java.time.LocalDate;
+
+public class RegisterDto {
+	
+private String firstName;
+
+private String lastName;
+
+private LocalDate dob;
+
+private String mobile;
+
+private String password;
+
+private String email;
+}
