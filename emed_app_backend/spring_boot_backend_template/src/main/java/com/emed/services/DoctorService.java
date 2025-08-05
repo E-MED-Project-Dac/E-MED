4e4ca9854be1845f00b2cf6067cdea3ad8f71094
89193-PulkitMehta <pulkitt522@gmail.com>
2025-08-05 10:31:14 +0530

    DoctorServiceChanges

--- emed_app_backend/spring_boot_backend_template/src/main/java/com/emed/services/DoctorService.java
+++ emed_app_backend/spring_boot_backend_template/src/main/java/com/emed/services/DoctorService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.emed.dtos.ApiResponse;
 import com.emed.dtos.DoctorEditDto;
+import com.emed.entities.Doctor;
 import com.emed.dtos.DoctorDto;
 
 public interface DoctorService {
