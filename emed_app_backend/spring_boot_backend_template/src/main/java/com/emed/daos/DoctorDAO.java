243610bcdebda6033dac26af97516fb641f48aff
89193-PulkitMehta <pulkitt522@gmail.com>
2025-08-05 10:12:46 +0530

    GetAllDoctors

--- emed_app_backend/spring_boot_backend_template/src/main/java/com/emed/daos/DoctorDAO.java
+++ emed_app_backend/spring_boot_backend_template/src/main/java/com/emed/daos/DoctorDAO.java
@@ -1,8 +1,9 @@
 package com.emed.daos;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.emed.entities.Doctor;
 
-public interface DoctorDAO extends JpaRepository<Doctor, Long>{
-
+public interface DoctorDAO extends JpaRepository<Doctor, Long> {
+    List<Doctor> findByIsVerifiedFalseAndIsDeletedFalse();
 }
--- emed_app_backend/spring_boot_backend_template/src/main/java/com/emed/services/DoctorServiceImp.java
+++ emed_app_backend/spring_boot_backend_template/src/main/java/com/emed/services/DoctorServiceImp.java
@@ -1,6 +1,6 @@
 package com.emed.services;
 
-
+import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,37 +8,47 @@ import com.emed.custom_exceptions.ResourceNotFoundException;
 import com.emed.daos.DoctorDAO;
 import com.emed.dtos.ApiResponse;
 import com.emed.dtos.DoctorEditDto;
-import com.emed.dtos.RegisterDTO;
+import com.emed.dtos.DoctorDto;
 import com.emed.entities.Doctor;
 import com.emed.entities.DoctorBasicDetails;
 import com.emed.entities.DoctorClinicDetails;
-
 import lombok.AllArgsConstructor;
 
-@Service @Transactional @AllArgsConstructor
+@Service
+@Transactional
+@AllArgsConstructor
 public class DoctorServiceImp implements DoctorService {
 
 	private final DoctorDAO doctorDao;
 	private final ModelMapper modelMapper;
-	
+
 	@Override
 	public ApiResponse removeDoctor(Long doctorId) {
 
-		Doctor doctor = doctorDao.findById(doctorId).orElseThrow(() -> new ResourceNotFoundException("Doctor not found"));
+		Doctor doctor = doctorDao.findById(doctorId)
+				.orElseThrow(() -> new ResourceNotFoundException("Doctor not found"));
 		doctor.setDeleted(true);
 		return new ApiResponse("Doctor deleted successfully");
 	}
 
-
 	@Override
 	public ApiResponse updateDoctor(DoctorEditDto doctorEditDto) {
-		Doctor existingDoctor = doctorDao.findById(doctorEditDto.getDoctorId()).orElseThrow(()-> new RuntimeException("Doctor Not Found"));
-	    modelMapper.map(doctorEditDto, existingDoctor);
-	    DoctorBasicDetails basicDetails =  modelMapper.map(doctorEditDto, DoctorBasicDetails.class);
-	    DoctorClinicDetails clinicDetails =  modelMapper.map(doctorEditDto, DoctorClinicDetails.class);
-	    existingDoctor.setBasicDetails(basicDetails);
-	    existingDoctor.setClinicDetails(clinicDetails);
+		Doctor existingDoctor = doctorDao.findById(doctorEditDto.getDoctorId())
+				.orElseThrow(() -> new RuntimeException("Doctor Not Found"));
+		modelMapper.map(doctorEditDto, existingDoctor);
+		DoctorBasicDetails basicDetails = modelMapper.map(doctorEditDto, DoctorBasicDetails.class);
+		DoctorClinicDetails clinicDetails = modelMapper.map(doctorEditDto, DoctorClinicDetails.class);
+		existingDoctor.setBasicDetails(basicDetails);
+		existingDoctor.setClinicDetails(clinicDetails);
 		return new ApiResponse("Updated successfully...!");
+	}
+
+	@Override
+	public List<DoctorDto> getAllDoctors() {
+		return doctorDao.findByIsVerifiedFalseAndIsDeletedFalse()
+				.stream()
+				.map(entity -> modelMapper.map(entity, DoctorDto.class))
+				.toList();
 	}
 
 }
