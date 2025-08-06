69c634d1b69012b99f31dd12b9a4a8ac95ea84a7
saransh <saranshvats041@gmail.com>
2025-08-06 14:43:22 +0530

    availability-integration

---
+++ emed_app_frontend/src/services/doctor.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import {config} from './../config';
+
+export async function updateAvailability(){
+    try{
+        let url = `${config.serverURL}/doctorHome/availability`
+
+        const response = await axios.put(url)
+        if(response.status >=200 && response.status < 300){
+            return response
+        }
+        else{
+            throw new Error(response.data?.message || 'Update failed');
+        }
+    }catch(error){
+        console.error('error: ',error);
+
+        throw error.response?.data || error.message || 'Unknown error';
+    }
+}
